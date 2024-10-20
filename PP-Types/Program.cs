1382b3e73fec7df3d2342b77e38945e9fd3dd491
Darya <225284@student.uek.krakow.pl>
2024-10-20 22:35:51 +0200

    homework

--- PP-Types/Program.cs
+++ PP-Types/Program.cs
@@ -1,6 +1,4 @@
-﻿// Write required code.
-
-// Data - do not change it in code!
+﻿// Data - do not change it in code!
 string[] names = {
     "Mickey Mouse", "Minnie Mouse", "Donald Duck", "Goofy", "Pluto", "Daisy Duck", "Simba", "Nala", 
     "Timon", "Pumbaa", "Mufasa", "Ariel", "Flounder", "Sebastian", "Ursula", "Belle", "Beast", "Gaston", 
@@ -8,34 +6,62 @@ string[] names = {
     "Olaf", "Moana", "Maui", "Hercules"
 };
 
-
 // Print all array elements, *perLine* elements per one line
 // After all elements except last one should be ", " - also on the end of lines.
 // After last element should be ".".
 void PrintGroups(string[] t, int perLine)
 {
+    for (int i = 0; i < t.Length; i++)
+    {
+        Console.Write(t[i]);
 
-    // Write required code.
+        if (i == t.Length - 1)
+        {
+            Console.Write(".");
+        }
+        else if ((i + 1) % perLine == 0)
+        {
+            Console.WriteLine(",");
+        }
+        else
+        {
+            Console.Write(", ");
+        }
+
+    }
 
 }
-
 
 // Print all array elements in *perLine* columns.
 // Each column must have given *width* (number of chars).
 // Columns should be separated by "| ".
 // If element is too long it should be trimmed.
-
 void PrintColumns(string[] t, int perLine, int width)
 {
-
-    // Write required code.
+    for (int i = 0; i < t.Length; i++)
+    {
+        if (t[i].Length > width)
+        {
+            Console.Write(t[i].Substring(0, width));
+        }
+        else
+        {
+            Console.Write(t[i].PadRight(width));
+        }
+        if ((i + 1) % perLine == 0)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.Write("| ");
+        }
+    }
 
 }
 
-
 // Test how your functions work. 
 // You can temprary comment some lines not needed for current testing.
-
 
 Console.WriteLine("\nPrintGroups(names, 3):\n");
 PrintGroups(names, 3);
